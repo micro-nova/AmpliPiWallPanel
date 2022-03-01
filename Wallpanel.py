419b4a4a39f73c4be9063c88fcb62bad4c98907f
Klayton Smith II <klayton@micro-nova.com>
2022-03-01 15:13:58 -0500

    working version

--- Wallpanel.py
+++ Wallpanel.py
@@ -1,16 +1,15 @@
-from curses import baudrate
 import urequests
 import json
 from machine import Pin, UART
 import network
 
 # debugUart = UART(0, baudrate=115200, tx=1, rx=3)
-tftUart = UART(2, baudrate=115200, tx=17, rx=16)
+tftUart = UART(2, baudrate=115200, tx=16, rx=17)
 tftReset = Pin(4, Pin.OUT)
 
 #constants for temporarily hardcoded stuff
 IP = "192.168.0.195"
-ZONE_ID = 1
+ZONE_ID = 4
 WIFI_SSID = "home_2G"
 WIFI_PASSWD = "***REMOVED***"
 
@@ -21,22 +20,21 @@ PREV_BUTTON_ID = 3
 SONG_NAME_VAR = "gname"
 ARTIST_NAME_VAR = "gartist"
 
-def wifi_connect():
-    
-
-
 def get_zone(zid):
-    response = json.loads(urequests.get(f'{IP}/api/zones/{zid}'))
+    response = json.loads(urequests.get(f'http://{IP}/api/zones/{zid}').text)
+    print(response)
     return response
 
 
 def get_source(sid):
-    response = json.loads(urequests.get(f'{IP}/api/sources/{sid}'))
+    response = json.loads(urequests.get(f'http://{IP}/api/sources/{sid}').text)
+    print(response)
     return response
 
 
 def command_stream(sid, cmd):
-    urequests.get(f'{IP}/api/{sid}/{cmd}')
+    response = json.loads(urequests.post(f'http://{IP}/api/streams/{sid}/{cmd}').text)
+    print(response)
 
 
 def send_title(title):
@@ -55,6 +53,7 @@ def send_artist(artist):
 
 def on_play():
     command_stream(stream_id, "play")
+    print("playing")
 
 
 def on_pause():
@@ -81,8 +80,8 @@ def get_stream_id_from_zone(zid):
 
 
 # initial startup stuff
-print('resetting screen')
-tftReset.on()
+print('resetting screen...')
+tftReset.value(0)
 
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
@@ -91,25 +90,32 @@ if not wlan.isconnected():
     wlan.connect(WIFI_SSID, WIFI_PASSWD)
     while not wlan.isconnected():
         pass
+    
+    print('connected!')
 
 # currently not doing any polling from amplipi at all
 stream_id = get_stream_id_from_zone(ZONE_ID)
+
+print(f"stream id is: {stream_id}")
 
 is_playing = True
 
 send_artist("test artist")
 send_title("test title")
 
-message = ''
+message = b''
 while True:
     if tftUart.any():
         message += tftUart.read()
+        print("read stuff in!")
 
-        if message[-3:] == chr(0xff)*3:
-            print("message recieved!")
+        if message[-3:] == bytes([0xff, 0xff, 0xff]):
+            print(f"message recieved!\n{message}")
 
-            if message[0] == chr(0x65) and message[3] == chr(0x00):
+            if message[0] == 0x65 and message[3] == 0x00:
                 id = message[2]
+
+                print("valid message")
 
                 if id == PLAY_BUTTON_ID:
                     if is_playing:
@@ -125,4 +131,4 @@ while True:
                 if id == PREV_BUTTON_ID:               
                     on_prev()
 
-            message == ''
+            message = b''
