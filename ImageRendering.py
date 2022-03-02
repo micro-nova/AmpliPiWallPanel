4e5806305993f814d75608321a657ed61f7a7af8
Klayton Smith II <klayton@micro-nova.com>
2022-03-02 12:59:46 -0500

    added new jpeg decoder an rebased

--- ImageRendering.py
+++ ImageRendering.py
@@ -1,12 +1,10 @@
-import ujpeg
-from ulab import numpy as np
-
+from decode import jpeg
+import DisplaySerial
 
 # https://github.com/daniel-falk/ujpeg
 # image is a jpeg
 def draw_image(image, tftUart):
-    r, g, b = ujpeg.decode(image)
-    w, h = r.shape
+    w, h, data = jpeg.decode(image)
 
     b5shift = 2**(8-5)
     b6shift = 2**(8-6)
@@ -14,13 +12,13 @@ def draw_image(image, tftUart):
     for y in range(w):
         for x in range(h):
             # fill x, y, 1, 1, rgb
-
+            r, g, b = data[y*w+x]
             # convert RGB888 TO RGB565
-            rshifted = round(r[y][x] / b5shift)
-            gshifted = round(g[y][x] / b6shift)
-            bshifted = round(b[y][x] / b5shift)
+            rshifted = round(r / b5shift)
+            gshifted = round(g / b6shift)
+            bshifted = round(b / b5shift)
             # color = round(rshifted + gshifted * b5shift + bshifted * b5shift * b6shift)
             color = rshifted + (gshifted << 5) + (bshifted << 11)
-            tftUart.write(f'fill {x}, {y}, 1, 1, {color}')
+            DisplaySerial.uartWrite(f'fill {x}, {y}, 1, 1, {color}')
 
 
